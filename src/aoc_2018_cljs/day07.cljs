e11b9e575e9b42fc24ff7ff1e15a731f57328e94
Jeffrey Simon <jdsimon@gmail.com>
2018-12-14 19:56:41 -0500

    Day 7, Part 2

--- src/aoc_2018_cljs/day07.cljs
+++ src/aoc_2018_cljs/day07.cljs
@@ -2,6 +2,7 @@
   (:require [aoc-2018-cljs.util :refer [line-seq-resource]]
             [clojure.set :as set]))
 
+
 (defn parse-lines [filename]
   (map (fn [line]
          (let [re #"Step (\w) must be finished before step (\w) can begin\."
@@ -39,26 +40,89 @@
   (set/subset? (get dependencies node #{nil}) done?))
 
 
-(defn find-next [{:keys [nodes] :as g} done?]
-  (first (sort (filter (fn [node]
-                         (and (dependencies-met? g done? node)
-                              (not (contains? done? node))))
-                       nodes))))
+(defn find-next-tasks [{:keys [nodes] :as g} done?]
+  (sort (filter (fn [node]
+                  (and (dependencies-met? g done? node)
+                       (not (contains? done? node))))
+                nodes)))
+
 
 (defn part-one [filename]
   (let [g (build-graph (parse-lines filename))]
     (loop [steps []
            done? #{:start}]
-      (let [next-step (find-next g done?)]
+      (let [next-step (first (find-next-tasks g done?))]
         (if (nil? next-step)
           (apply str steps)
           (recur (conj steps next-step)
                  (into done? next-step)))))))
 
 
+(defn task-seconds [task]
+  (+ 60 (inc (- (.charCodeAt task) (.charCodeAt "A")))))
+
+
+(defn assign-tasks
+  [worker-state current-time avail-tasks]
+  (let [avail-workers (->> worker-state
+                           (filter (comp nil? second))
+                           (map first))]
+    (reduce (fn [worker-state [avail-worker avail-task]]
+              (assoc worker-state avail-worker {:task avail-task :time-started current-time}))
+            worker-state
+            (map #(vector %1 %2) avail-workers avail-tasks))))
+
+
+(defn busy-workers [worker-state]
+  (filter (comp not nil? second) worker-state))
+
+
+(defn tasks-in-progress [worker-state]
+  (set (filter some? (map (comp :task second) worker-state))))
+
+
+(defn progress-done
+  [{:keys [worker-state] :as state} current-time]
+  (let [times-up? (fn [task time-started]
+                    (= (task-seconds task) (- current-time time-started)))]
+    (reduce (fn [state [worker {:keys [task time-started]}]]
+              (if (times-up? task time-started)
+                (-> (assoc-in state [:worker-state worker] nil)
+                    (update :done? #(into % task)))
+                state))
+            state
+            (busy-workers worker-state))))
+
+
+
+(defn initialize-state [workers]
+  {:worker-state (into {} (map #(vector % nil) (range workers)))
+   :done? #{:start}})
+
+
+(defn part-two [filename workers]
+  (let [g (build-graph (parse-lines filename))]
+    (loop [state (initialize-state workers)
+           current-time 0]
+      (let [{:keys [worker-state done?]} (progress-done state current-time)
+            tasks-in-progress (tasks-in-progress worker-state)
+            avail-tasks (sort (remove tasks-in-progress (find-next-tasks g done?)))
+            worker-state+ (assign-tasks worker-state current-time avail-tasks)]
+        (if (not (seq (busy-workers worker-state+)))
+          current-time
+          (recur {:worker-state worker-state+ :done? done?}
+                 (inc current-time)))))))
+
+
+
+
 (comment
 
   ;; Part 1
-  (part-one "day07.txt"))
+  (part-one "day07.txt")
+
+
+  ;; Part 2
+  (part-two "day07.txt" 5))
 
 
